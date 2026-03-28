7b0a1758a61af0fe7ff90f826e2e5a2ad5cdca4d
joel-silva-design <joel09.silva.js@gmail.com>
2026-03-28 11:50:33 -0300

    Add files via upload

--- script9.js
+++ script9.js
@@ -1,5 +1,5 @@
-let altura = 1.65;
-let peso = 75;
+let altura = 0;
+let peso = 79;
 
 let imc = (peso / altura**2);
 
@@ -7,12 +7,27 @@ if (imc > 0 && imc < 18.5){
   console.log("Abaixo do Peso");
 } else if (imc >= 18.5 && imc <= 24.9){
   console.log("Peso Normal");
-} else if (imc >= 25 && imc <= 29.9){
+} else if (imc >= 25.0 && imc <= 29.9){
   console.log("Sobrepeso");
-} else if (imc >= 30){
-  console.log("Obesidade");
+} else if (imc >= 30.0 && imc <= 34.9){
+  console.log("Obesidade Leve");
+} else if (imc >= 35.0 && imc <= 39.9){
+  console.log("Obesidade Moderada");
+} else if (imc >= 40.0 && imc <= 50.0){
+  console.log("Obesidade Mórbida");
+} else if (imc > 50.0 && imc != 'Infinity') {
+  console.log("Super Obesidade");
+} else {
+  console.log("IMC invalido");
 }
 
+let info = `Seu imc é ${imc.toFixed(2)}`;
+
+if (imc > 0 && imc != 'Infinity'){
+console.log (info);
+} else {
+  console.log("Digite valores validos para Altura e Peso"); 
+}
 const botao = document.querySelector('.cartao__botao');
 const cartao = document.querySelector('.cartao');
 botao.addEventListener('click', () => {
