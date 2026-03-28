6e330d78e7bad49117d61b886cb1a83c8ad81e07
joel-silva-design <joel09.silva.js@gmail.com>
2026-03-27 21:49:57 -0300

    Add files via upload

--- script9.js
+++ script9.js
@@ -1,10 +1,16 @@
-let numero = 30;
-let resto = (numero % 2);
+let altura = 1.65;
+let peso = 75;
 
-if (resto === 0) {
-  console.log("O número é par");
-} else if (resto === 1) {
-  console.log("O número é impar");
+let imc = (peso / altura**2);
+
+if (imc > 0 && imc < 18.5){
+  console.log("Abaixo do Peso");
+} else if (imc >= 18.5 && imc <= 24.9){
+  console.log("Peso Normal");
+} else if (imc >= 25 && imc <= 29.9){
+  console.log("Sobrepeso");
+} else if (imc >= 30){
+  console.log("Obesidade");
 }
 
 const botao = document.querySelector('.cartao__botao');
